444bc815c6b016eabf5c70dac0821e4b14aa586b
Pedro Zambão <pedrozambaoo@gmail.com>
2022-03-02 18:07:37 -0300

    adicao validacao (ainda em ingles)

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -1,2 +1,5 @@
 class Aluno < ActiveRecord::Base
+  validates :turma, :nome, :email, :prontuario, presence:true
+  validates_length_of :nome, minimum: 4, maximum: 50
+  validates :prontuario, uniqueness: true
 end
